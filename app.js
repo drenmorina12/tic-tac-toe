071b685cdda8e1727666f986827c56210b4f07df
Dren Morina <drenmorina321@gmail.com>
2024-09-25 01:26:08 +0200

    Create main structure for GameController

--- app.js
+++ app.js
@@ -23,4 +23,34 @@ function createPlayer(name, token) {
   return { name, token };
 }
 
-function GameController() {}
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) {
+  const board = Gameboard();
+
+  const players = [
+    createPlayer(playerOneName, "x"),
+    createPlayer(playerTwoName, "circle"),
+  ];
+
+  let currentPlayer = players[0];
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  const switchPlayerTrun = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  };
+
+  const playRound = (cell) => {};
+
+  const checkWin = (currentPlayer) => {};
+
+  const isDraw = () => {};
+
+  const gameOver = () => {};
+
+  return { playRound, getBoard: board.getBoard, getCurrentPlayer };
+}
+
+GameController();
